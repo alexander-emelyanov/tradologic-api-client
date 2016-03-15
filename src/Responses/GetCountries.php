f61ab11301c262aedee6ca4e8f0571af89fe5a23
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2016-03-15 15:02:34 +0100

    Comment adjusted.

--- src/Responses/GetCountries.php
+++ src/Responses/GetCountries.php
@@ -22,7 +22,7 @@ class GetCountries extends Response
         if ($this->isSuccess()) {
             if (!empty($this->data[static::FIELD_DATA])) {
                 foreach ($this->data[static::FIELD_DATA] as $countryInfo) {
-                    // Two countries from TradoLogic API have currency.
+                    // Two countries from TradoLogic API have not currency.
                     // These countries: "Unknown" and "Antarctica". Real "Tear 1" countries with fat traffic of penguins and nobodies...
                     // But we should skip them...
                     if (isset($countryInfo[static::FIELD_CURRENCY]) && $countryInfo[static::FIELD_CURRENCY]){
